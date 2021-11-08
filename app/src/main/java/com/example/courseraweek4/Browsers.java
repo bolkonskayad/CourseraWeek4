c33df2dd33cd22cd6947d3201189d5607988c1c8
bolkonskayad <chibisova.d@inbox.ru>
2021-11-08 20:46:03 +0300

    Поиск и меню 2.0

--- app/src/main/java/com/example/courseraweek4/Browsers.java
+++ app/src/main/java/com/example/courseraweek4/Browsers.java
@@ -1,13 +1,5 @@
 package com.example.courseraweek4;
 
-import java.util.Arrays;
-import java.util.List;
-
-public class Browsers {
-
-    public static final String GOOGLE = "Google";
-    public static final String YANDEX = "Яндекс";
-    public static final String BING = "Bing";
-
-    private static List<String> companies = Arrays.asList(GOOGLE, YANDEX, BING);
+public enum Browsers {
+    GOOGLE, YANDEX, BING
 }
